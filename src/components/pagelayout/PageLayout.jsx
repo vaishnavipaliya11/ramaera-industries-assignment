df9ac3d7548b0728481dfd59e9588f6cc5e4b8b5
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2022-11-18 22:21:37 +0530

    chore : files added

--- src/components/pagelayout/PageLayout.jsx
+++ src/components/pagelayout/PageLayout.jsx
@@ -1,9 +1,15 @@
 import { Header } from "../header/Header"
 import "../../styles.css"
+import { Sidebar } from "../sidebar/Sidebar"
+import { DetailCard } from "../productsDetail/DetailCard"
 export const PageLayout = () =>{
     return(
         <div className="page-layout">
             <Header/>
+            <div className="sub-layout">
+            <Sidebar/>
+            <DetailCard/>
+            </div>
         </div>
     )
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,30 @@
-export const Sidebar =() =>{
-    return(
-        <div>Sidebar</div>
-    )
-}
+export const Sidebar = () => {
+  const sideBarData = [
+    { name: "Profile", icon: "/assets/user.svg" },
+    {
+      name: "Projects",
+      icon: "/assets/projects.svg",
+      dropDown:"/assets/"
+    },
+    {
+      name: "Finance",
+      icon: "/assets/finance.svg",
+    },
+    {
+      name: "Support",
+      icon: "/assets/info.svg",
+    },
+  ];
+  return (
+    <div>
+      {sideBarData.map((item) => {
+        return (
+          <div className="common-flex-row center sidebar-mapped-items">
+            <img src={item.icon} alt="item-icon" />
+            <span>{item.name}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
