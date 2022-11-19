a2efd215035b956c0aa9fff3a2d459c93920d3fa
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2022-11-19 10:34:48 +0530

    style: sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,27 +1,32 @@
+import "../sidebar/sidebar.css"
+import "../../utils.css"
 export const Sidebar = () => {
   const sideBarData = [
-    { name: "Profile", icon: "/assets/user.svg" },
+    { name: "Profile", icon: "/assets/user.svg", dropDown:"" },
     {
       name: "Projects",
       icon: "/assets/projects.svg",
-      dropDown:"/assets/"
+      dropDown:"/assets/drop.svg"
     },
     {
       name: "Finance",
       icon: "/assets/finance.svg",
+      dropDown:"/assets/drop.svg"
     },
     {
       name: "Support",
       icon: "/assets/info.svg",
+      dropDown:"/assets/drop.svg"
     },
   ];
   return (
-    <div>
+    <div className="sidebar-container common-flex-col">
       {sideBarData.map((item) => {
         return (
           <div className="common-flex-row center sidebar-mapped-items">
-            <img src={item.icon} alt="item-icon" />
-            <span>{item.name}</span>
+            <img src={item.icon} alt="item-icon" className="pd-sm"/>
+            <span className="pd-sm">{item.name}</span>
+            <img src={item?.dropDown}  className="pd-sm"/>
           </div>
         );
       })}
